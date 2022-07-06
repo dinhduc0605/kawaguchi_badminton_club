ce8cf5dce270398296fa9e30cddf9a54481cf880
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2022-07-07 01:05:13 +0900

    manage event

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,31 @@
+class Admin::EventsController < ApplicationController
+  def index
+    @events = Event.all.order(:id)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event = Event.create!(event_params)
+
+    redirect_to admin_events_path
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update!(event_params)
+
+    redirect_to admin_events_path
+    
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :note, :start_time, :end_time, :kind)
+  end
+end
